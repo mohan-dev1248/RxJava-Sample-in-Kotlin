0b81c45fe4cd36e1b6c80e5a024f10a5eea80b0e
mohanakrishna <mohanakrishna96@gmail.com>
2020-03-03 15:09:13 +0530

    Operator examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Timed
+import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.concurrent.thread
+
 
 fun main() {
 
@@ -55,4 +60,60 @@ fun printNaturalNumbersWithTimer(){
     log("Starting")
     naturalNumbersTimeOut.timeout(100, TimeUnit.MILLISECONDS).subscribe { num -> log("$num") }
     log("exit")
+}
+
+fun mapFilterExample(){
+    Observable
+        .just(8, 9, 10)
+        .doOnNext { i: Int -> println("A: $i") }
+        .filter { i: Int -> i % 3 > 0 }
+        .doOnNext { i: Int -> println("B: $i") }
+        .map { i: Int -> "#" + i * 10 }
+        .doOnNext { s: String -> println("C: $s") }
+        .filter { s: String -> s.length < 4 }
+        .subscribe { s: String -> println("D: $s") }
+}
+
+fun repeatFirstEightAlphabetsEightTimes(){
+    val oneToEight = Observable.range(1, 8)
+    val ranks = oneToEight
+        .map { obj: Int -> obj.toString() }
+    val files = oneToEight
+        .map { x: Int -> 'a'.toInt() + x - 1 }
+        .map { ascii: Int -> ascii.toChar() }
+        .map { ch: Char? ->
+            (ch!!).toString()
+        }
+    files.flatMap { file: String ->
+            ranks.map { rank: String -> file + rank }
+        }.subscribe{ s: String -> println(s)}
+}
+
+
+fun zipExample(){
+    val red = Observable.interval(10, TimeUnit.MILLISECONDS)
+    val green = Observable.interval(10, TimeUnit.MILLISECONDS)
+    Observable.zip(
+        red.timestamp(),
+        green.timestamp(),
+        BiFunction<Timed<Long>, Timed<Long>, Any> { r: Timed<Long>, g: Timed<Long> -> r.time() - g.time() }
+    ).forEach { x: Any? -> println(x) }
+}
+
+fun scanExample(){
+    val progress = Observable.just(10,14, 12, 15, 14,10)
+
+    val totalProgress = progress.scan{ total, chunk -> total + chunk}.subscribe { it -> println("$it") }
+}
+
+fun reduceExample(){
+    val all: Disposable? = Observable
+        .range(10, 20)
+        .reduce(
+            ArrayList(),
+            { list: ArrayList<Any?>, item: Int? ->
+                list.add(item)
+                list
+            }
+        ).subscribe { it -> it.forEach { item -> println("$item") } }
 }
